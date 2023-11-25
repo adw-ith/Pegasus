a735a75c452cca6d7d664bdf8efab6c45f689630
Adwaith Raj <adwaithraj45654@gmail.com>
2023-11-25 09:12:37 +0530

    commit2

--- server.js
+++ server.js
@@ -5,22 +5,24 @@ const path = require('path');
 
 const PORT = 3000;
 
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-let receivedDataFromESP32 = ''; 
+let sensorData = ''; // Variable to store received sensor data
 
-app.post('/esp32data', (req, res) => {
-  receivedDataFromESP32 = req.body.data;
-  console.log('Data received from ESP32:', receivedDataFromESP32);
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/data', (req, res) => {
+  sensorData = req.body.sensorValue; // Assuming ESP32 sends 'sensorValue'
+  console.log('Data received from ESP32:', sensorData);
   res.send('Data received successfully');
 });
 
 app.get('/getdata', (req, res) => {
-  res.json({ data: receivedDataFromESP32 });
+  res.json({ sensorData });
 });
-
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
